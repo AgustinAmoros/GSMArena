80345ffd535edf5747d2c626f5d48d7a81b18ac2
AmorosAgustin <agusela_2000@hotmail.com>
2022-01-02 16:24:08 -0300

    push 1

--- src/test/java/com/solvd/gsmarena/tests/GSMArenaTests.java
+++ src/test/java/com/solvd/gsmarena/tests/GSMArenaTests.java
@@ -9,11 +9,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class GSMArenaTests implements IAbstractTest, ISignUpService {
 
-    WebDriver driver = new ChromeDriver();
+   public WebDriver driver;
+   @BeforeTest
+           public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "/Applications/chromedriver");
+       driver = new ChromeDriver();
+       driver.get("http://www.gsmarena.com");
+    }
 
     @Test
     public void verifyHomePageWorkflow(){
